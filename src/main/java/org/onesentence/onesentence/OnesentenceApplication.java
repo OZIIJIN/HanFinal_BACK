dd87ff3ade42a5c090147a6598eed076c8d0d64b
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-05-31 17:30:05 +0900

    git init

---
+++ src/main/java/org/onesentence/onesentence/OnesentenceApplication.java
@@ -0,0 +1,13 @@
+package org.onesentence.onesentence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnesentenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnesentenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/onesentence/onesentence/OnesentenceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.onesentence.onesentence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnesentenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

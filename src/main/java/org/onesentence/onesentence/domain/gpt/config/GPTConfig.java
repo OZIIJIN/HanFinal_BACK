95a7f9e217c335eb8755bd254b328035c1081c2f
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-05-31 20:00:51 +0900

    feat: feat: GPT api 연결 기능 구현 - Config

---
+++ src/main/java/org/onesentence/onesentence/domain/gpt/config/GPTConfig.java
@@ -0,0 +1,26 @@
+package org.onesentence.onesentence.domain.gpt.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class GPTConfig {
+
+	@Value("${openai.api.key}")
+	private String key;
+
+	@Bean
+	public RestTemplate template() {
+		RestTemplate template = new RestTemplate();
+
+		template.getInterceptors().add((request, body, execution) -> {
+			request.getHeaders().add(
+				"Authorization"
+				,"Bearer "+key);
+			return execution.execute(request,body);
+		});
+		return template;
+	}
+}

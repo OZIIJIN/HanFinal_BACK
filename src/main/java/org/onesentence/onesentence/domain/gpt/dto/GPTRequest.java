df8867f2e5a5d8e4268957c7affe670da6ad228c
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-06-08 16:32:31 +0900

    refactor: role 구분

--- src/main/java/org/onesentence/onesentence/domain/gpt/dto/GPTRequest.java
+++ src/main/java/org/onesentence/onesentence/domain/gpt/dto/GPTRequest.java
@@ -21,7 +21,12 @@ public class GPTRequest {
 	public GPTRequest(String model, String prompt, int temperature, int maxTokens, int topP, int frequencyPenalty, int presencePenalty) {
 		this.model = model;
 		this.messages = new ArrayList<>();
-		this.messages.add(new Message("user", prompt + " 이 문장 일정 등록하려고 하는데 일시, 사람, 일정 내용으로 JSON 형태로 만들어줘"));
+		this.messages.add(new Message("system", "assistant는 일정 관리 해주는 앱이야."));
+		this.messages.add(new Message("user", prompt));
+		this.messages.add(new Message("system", " 이 문장 일정 등록하려고 하는데 일정 내용, 카테고리로 JSON 형태로 분류 해줘. 예시: {\n"
+			+ "  \"title\": \"테니스\",\n"
+			+ "  \"category\": \"운동\",\n"
+			+ "}"));
 		this.temperature = temperature;
 		this.maxTokens = maxTokens;
 		this.topP=topP;

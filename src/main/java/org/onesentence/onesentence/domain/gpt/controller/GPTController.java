06e74a6da2e60cae4c7db5ffdeff60c3a833eed8
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-06-10 10:07:31 +0900

    refactor: service 도입

--- src/main/java/org/onesentence/onesentence/domain/gpt/controller/GPTController.java
+++ src/main/java/org/onesentence/onesentence/domain/gpt/controller/GPTController.java
@@ -3,6 +3,7 @@ package org.onesentence.onesentence.domain.gpt.controller;
 import lombok.RequiredArgsConstructor;
 import org.onesentence.onesentence.domain.gpt.dto.GPTRequest;
 import org.onesentence.onesentence.domain.gpt.dto.GPTResponse;
+import org.onesentence.onesentence.domain.gpt.service.GptService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,29 +16,12 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/api/v1/chatGpt")
 public class GPTController {
 
-	@Value("${openai.model:gpt-3.5-turbo}")
-	private String model;
-
-	@Value("${openai.api.url:https://api.openai.com/v1/chat/completions}")
-	private String apiUrl;
-
-	private final RestTemplate restTemplate;
+	private final GptService gptService;
 
 	@GetMapping("/chat")
 	public String chat(@RequestParam("prompt") String prompt){
 
-		GPTRequest request = new GPTRequest(
-			model,prompt,1,256,1,2,2);
-
-		GPTResponse gptResponse = restTemplate.postForObject(
-			apiUrl
-			, request
-			, GPTResponse.class
-		);
-
-
-		return gptResponse.getChoices().get(0).getMessage().getContent();
-
+		return gptService.gptCall(prompt);
 
 	}
 }
--- src/main/java/org/onesentence/onesentence/domain/gpt/dto/GPTRequest.java
+++ src/main/java/org/onesentence/onesentence/domain/gpt/dto/GPTRequest.java
@@ -2,6 +2,7 @@ package org.onesentence.onesentence.domain.gpt.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -18,19 +19,21 @@ public class GPTRequest {
 	private int frequencyPenalty;
 	private int presencePenalty;
 
-	public GPTRequest(String model, String prompt, int temperature, int maxTokens, int topP, int frequencyPenalty, int presencePenalty) {
+	public GPTRequest(String model, String prompt, int temperature, int maxTokens, int topP,
+		int frequencyPenalty, int presencePenalty) {
 		this.model = model;
 		this.messages = new ArrayList<>();
 		this.messages.add(new Message("system", "assistant는 일정 관리 해주는 앱이야."));
 		this.messages.add(new Message("user", prompt));
-		this.messages.add(new Message("system", " 이 문장 일정 등록하려고 하는데 일정 내용, 카테고리로 JSON 형태로 분류 해줘. 예시: {\n"
-			+ "  \"title\": \"테니스\",\n"
-			+ "  \"category\": \"운동\",\n"
-			+ "}"));
+		this.messages.add(new Message("system", """
+			 이 문장 일정 등록하려고 하는데 일정 내용, 카테고리로 JSON 형태로 분류 해줘. 예시: {
+			  "title": "테니스",
+			  "category": "운동",
+			}"""));
 		this.temperature = temperature;
 		this.maxTokens = maxTokens;
-		this.topP=topP;
-		this.frequencyPenalty=frequencyPenalty;
+		this.topP = topP;
+		this.frequencyPenalty = frequencyPenalty;
 		this.presencePenalty = presencePenalty;
 	}
 

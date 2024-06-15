2dbdd619e4b22d9c53ca38bff997961923b71967
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-06-15 16:15:43 +0900

    feat: todo 수정 기능 추가

--- src/main/java/org/onesentence/onesentence/domain/todo/controller/TodoController.java
+++ src/main/java/org/onesentence/onesentence/domain/todo/controller/TodoController.java
@@ -2,13 +2,11 @@ package org.onesentence.onesentence.domain.todo.controller;
 
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.onesentence.onesentence.domain.todo.dto.TodoRequest;
 import org.onesentence.onesentence.domain.todo.service.TodoService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +18,13 @@ public class TodoController {
 	@PostMapping
 	public ResponseEntity<String> createTodo(@RequestBody TodoRequest request) {
 		Long todoId = todoService.createTodo(request);
+
+		return ResponseEntity.created(URI.create("/api/v1/todos/" + todoId)).build();
+	}
+
+	@PutMapping
+	public ResponseEntity<String> updateTodo(@RequestBody TodoRequest request, @PathVariable Long todoId) {
+		Long updatedTodoId = todoService.updateTodo(request, todoId);
 
 		return ResponseEntity.created(URI.create("/api/v1/todos/" + todoId)).build();
 	}
--- src/main/java/org/onesentence/onesentence/domain/todo/service/TodoService.java
+++ src/main/java/org/onesentence/onesentence/domain/todo/service/TodoService.java
@@ -1,8 +1,13 @@
 package org.onesentence.onesentence.domain.todo.service;
 
 import org.onesentence.onesentence.domain.todo.dto.TodoRequest;
+import org.onesentence.onesentence.domain.todo.entity.Todo;
 
 public interface TodoService {
 
 	Long createTodo(TodoRequest request);
+
+	Long updateTodo(TodoRequest request, Long todoId);
+
+	Todo findById(Long todoId);
 }

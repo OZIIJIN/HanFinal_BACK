847b57552ec1aebb4af8e2da42801ed1db790d06
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-06-15 16:04:10 +0900

    feat: category 수정 기능 추가

--- src/main/java/org/onesentence/onesentence/domain/category/service/CategoryService.java
+++ src/main/java/org/onesentence/onesentence/domain/category/service/CategoryService.java
@@ -1,8 +1,13 @@
 package org.onesentence.onesentence.domain.category.service;
 
 import org.onesentence.onesentence.domain.category.dto.CategoryRequest;
+import org.onesentence.onesentence.domain.category.entity.Category;
 
 public interface CategoryService {
 
 	Long createCategory(CategoryRequest request);
+
+	Long updateCategory(CategoryRequest request, Long categoryId);
+
+	Category findById(Long categoryId);
 }

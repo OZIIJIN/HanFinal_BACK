d2336ff2bdce74eed13c8ea99c6f5801705e33b8
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-06-23 23:55:29 +0900

    refactor: GPTRequest 수정 - system 텍스트 클래스 내 변수로 관리 및 메세지 구조 변경

--- src/main/java/org/onesentence/onesentence/domain/gpt/dto/GPTRequest.java
+++ src/main/java/org/onesentence/onesentence/domain/gpt/dto/GPTRequest.java
@@ -19,19 +19,20 @@ public class GPTRequest {
 	private int frequencyPenalty;
 	private int presencePenalty;
 
+	private static final String SYSTEM_MESSAGE =
+		"assistant는 일정 관리 해주는 앱이야. user 의 prompt 문장을 일정으로 등록하려는데 JSON 형태로 일정 내용, 카테고리, 날짜로 분류 해줘. 현재 날짜는 "
+			+ LocalDateTime.now() + "이야. 예시: {\n" +
+			"\t\t\t  \"title\": \"테니스\",\n" +
+			"\t\t\t  \"category\": \"운동\",\n" +
+			"\t\t\t  \"date\": \"2023-11-12T16:34:30.388\"\n" +
+			"\t\t\t}";
+
 	public GPTRequest(String model, String prompt, int temperature, int maxTokens, int topP,
 		int frequencyPenalty, int presencePenalty) {
 		this.model = model;
 		this.messages = new ArrayList<>();
-		this.messages.add(new Message("system", "assistant는 일정 관리 해주는 앱이야."));
+		this.messages.add(new Message("system", SYSTEM_MESSAGE));
 		this.messages.add(new Message("user", prompt));
-		this.messages.add(new Message("system",
-			"이 문장 일정 등록하려고 하는데 일정 내용, 카테고리, 날짜로 JSON 형태로 분류 해줘. 현재 날짜는 " + LocalDateTime.now()
-				+ "이야. 예시: {\n"
-				+ "\t\t\t  \"title\": \"테니스\",\n"
-				+ "\t\t\t  \"category\": \"운동\",\n"
-				+ "\t\t\t  \"date\": \"2023-11-12T16:34:30.388\"\n"
-				+ "\t\t\t}"));
 		this.temperature = temperature;
 		this.maxTokens = maxTokens;
 		this.topP = topP;

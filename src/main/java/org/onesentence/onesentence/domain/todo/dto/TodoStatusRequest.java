0281751f3ec647030d253f56512c92b5156c5ece
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-06-24 00:13:31 +0900

    feat: todo status 변경 기능 추가

---
+++ src/main/java/org/onesentence/onesentence/domain/todo/dto/TodoStatusRequest.java
@@ -0,0 +1,12 @@
+package org.onesentence.onesentence.domain.todo.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TodoStatusRequest {
+
+	private String status;
+
+}

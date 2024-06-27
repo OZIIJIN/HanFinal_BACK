1c214d832b5a2fd40efcbfdb75c9bdab0ab9d54a
OZIIJIN <dpwlsdlqma6@gmail.com>
2024-06-27 18:22:01 +0900

    feat: category 조회 기능 구현 - 단건 조회, 전체 조회

--- src/main/java/org/onesentence/onesentence/domain/category/service/CategoryService.java
+++ src/main/java/org/onesentence/onesentence/domain/category/service/CategoryService.java
@@ -1,6 +1,8 @@
 package org.onesentence.onesentence.domain.category.service;
 
+import java.util.List;
 import org.onesentence.onesentence.domain.category.dto.CategoryRequest;
+import org.onesentence.onesentence.domain.category.dto.CategoryResponse;
 import org.onesentence.onesentence.domain.category.entity.Category;
 
 public interface CategoryService {
@@ -12,4 +14,8 @@ public interface CategoryService {
 	Category findById(Long categoryId);
 
 	void deleteCategory(Long categoryId);
+
+	List<CategoryResponse> getAllCategories();
+
+	CategoryResponse getCategory(Long categoryId);
 }
